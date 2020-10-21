2f40096fb53ca25d351b64a0787c903c942ecc09
JaelCcz <jaelcchz@outlook.com>
2020-10-20 23:43:54 -0500

    add function lower

--- practica_composer/index.php
+++ practica_composer/index.php
@@ -1,9 +1,11 @@
 <?php
-
+    // carga automatica de clases
     require __DIR__ . '/vendor/autoload.php';
 
-
+    // Mandar a llamar por clase
     // echo Text\ Format::upperText('Hola este es mi primer paquete :D');
     
-    // funcion 
-     echo upper('Hola este es mi primer paquete :D');
+
+    // Mandar a llamar por funcion
+    // echo upper('Hola este es un texto en mayusculas');
+    echo lower('Hola este es un texto en minuscula');
